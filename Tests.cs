8c0116dad121df5390d474df187cddf7dcda944e
StephenDBaker <stephenbaka@gmail.com>
2021-01-27 15:14:57 -0700

    .

--- Tests.cs
+++ Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,27 +7,57 @@ namespace GameOfWar
 {
     public class Tests
     {
+        private Game classUnderTest;
+        private List<int> deck;
+
+        [SetUp]
+        public void Setup()
+        {
+            classUnderTest = new Game();
+            deck = classUnderTest.CreateDeck();
+        }
+
         [Test]
         public void Has_52_cards()
         {
-            var classUnderTest = new Game();
-            var deck = classUnderTest.CreateDeck();
             Assert.That(deck.Count, Is.EqualTo(52));
         }
 
         [Test]
-        [TestCase(2)]
-        [TestCase(4)]
-        [TestCase(6)]
-        [TestCase(14)]
-        public void Can_create_a_deck_with_real_cards(int value)
+        [TestCase(2, 4)]
+        [TestCase(4, 4)]
+        [TestCase(6, 4)]
+        [TestCase(14, 4)]
+        [TestCase(15, 0)]
+        public void Can_create_a_deck_with_real_cards(int value, int expectedCount)
         {
-            var classUnderTest = new Game();
-            var deck = classUnderTest.CreateDeck();
-
-            Assert.That(deck.Count(a => a == value), Is.EqualTo(4));
+            Assert.That(deck.Count(a => a == value), Is.EqualTo(expectedCount));
         }
 
-                
+        [Test]
+        public void Something()
+        {
+            var copyOfDeck = deck.ToList();
+
+            //Loop 100 times
+            //Get a random value between 0 and 51
+            //Get a random value between 0 and 51
+            //Swap
+
+            for(int i = 0; i < 99; i++)
+            {
+                var r1 = new Random();
+                var indexA = r1.Next(0, 51);
+
+                var r2 = new Random();
+                var indexB = r2.Next(0, 51);
+
+                var valueBeingSwapped = copyOfDeck[indexA];
+                copyOfDeck[indexA] = copyOfDeck[indexB];
+                copyOfDeck[indexB] = valueBeingSwapped;
+            }
+
+            Assert.That(copyOfDeck.SequenceEqual(deck), Is.False);
+        }
     }
 }
